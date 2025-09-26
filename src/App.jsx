e5dfd18cf2367f5c1bbb96f5f33b3bbaf2649dc1
Sarvesh <sarvesh.kumar.jsr@gmail.com>
2025-09-26 14:08:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Cbc from "./allTopics/01_typesOfComponent/Cbc";
+import Fbc from "./allTopics/01_typesOfComponent/Fbc";
+import Card from "./components/Card"
+
+function App() {
+  return (
+    <>
+        {/* <Card/> */}
+        <Fbc/>
+        <Cbc/>
+    </>
+  )
+}
+
+export default App;  
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+//! REACT way
+import { createRoot } from "react-dom/client";
+import App from "./App";
+createRoot(document.getElementById("root")).render(<App/>);
+
+//! JS way
+// const div = document.getElementById("root")
+// const h1Tag = document.createElement("h1")
+// h1Tag.innerText = "Hello JS"
+// div.append(h1Tag)
+
